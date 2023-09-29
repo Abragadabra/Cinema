db04310faca66a1d81324f6ec585adef95b76d00
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2023-09-29 12:25:53 +0500

    добавление комментариев и изменения в коде
    - добавлен .gitignore, так как файл с макетом был перенесён внутрь проекта
    - дополнены и изменены комментарии в index.html
    - Добавлены комментарии в script.js, а также цена билета выведена отдельной константой
    - добавлен favicon на сайт

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,45 @@
-let totalSum = 0
+// rubbleSum - элемента с суммой
+// ticketPrice - цена одного билета
+// MegaArraySeats - массив сидений
+// MegaArrayIMG - массив изображений сидений
+const rubbleSum = document.querySelector('.fee-text-sum');
+const ticketPrice = 500;
+const MegaArraySeats = document.querySelectorAll('.seat');
+const MegaArrayIMG = document.querySelectorAll('.seat img');
 
-const rubbleSum = document.querySelector('.fee-text-sum')
-const MegaArraySeats = document.querySelectorAll('.seat')
-const MegaArrayIMG = document.querySelectorAll('.seat img')
+// totalSum - итоговая сумма
+let totalSum = 0;
 
+// Перебор массива изображений и выдача им атрибута draggable false
 MegaArrayIMG.forEach(element => {
     element.setAttribute('draggable', 'false')
 })
 
+// Перебор массива сидений
+// Если у сидения класс closed, то ничего не делаем
+// Если у сидения класс poked, то меняем сидение на обычно и вычитаем сумму
+// Иначе мы делаем сидение жёлтым и добавляем сумму
 MegaArraySeats.forEach(element => {
     element.addEventListener('click', function () {
         if (element.classList.contains('closed')) {
             
         }
         else if (element.classList.contains('poked')) {
-            element.classList.remove('poked')
-            element.innerHTML = `<img src="img/seat.svg">`
-            totalSum -= 800
+            element.classList.remove('poked');
+            element.innerHTML = `<img src="img/seat.svg">`;
+            totalSum -= ticketPrice;
         }
         else {
-            element.classList.add('poked')
-            element.innerHTML = `<img src="img/yellow-seat.svg">`
-            totalSum += 800
+            element.classList.add('poked');
+            element.innerHTML = `<img src="img/yellow-seat.svg">`;
+            totalSum += ticketPrice;
         }
-        updateSum()
+        // Обновляем элемент с суммой
+        updateSum();
     })
 });
 
+// Функция обновления элемента с суммой, которая заменяем текст на сумму
 function updateSum() {
-    rubbleSum.textContent = totalSum
+    rubbleSum.textContent = totalSum;
 }
